d2f2e08474d1c9b8cae2b7e50f8ff87408390556
optimisme <optimisme@gmail.com>
2023-04-14 17:03:09 +0200

    Increased number of polygons

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ ws.onMessage = (socket, id, obj) => {
     if (existingPolygonIndex !== -1) {
       poligons[existingPolygonIndex] = receivedPoligon;
     } else {
-      while (poligons.length > 25) {
+      while (poligons.length > 50) {
         poligons.shift();
       }
       poligons.push(receivedPoligon);
